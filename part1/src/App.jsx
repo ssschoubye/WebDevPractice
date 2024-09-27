a0da855f899d4eebfcf802daadba6e4eac81db1b
ssschoubye <116802763+ssschoubye@users.noreply.github.com>
2024-09-27 21:10:24 +0200

    part1 continuation

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -3,10 +3,27 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const App = () => {
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello {props.name} your age is {props.age}</p>
+      </div>
+  )
+}
+
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+  const name = 'Peter'
+  const age = 10
+  console.log(now, a +b)
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
